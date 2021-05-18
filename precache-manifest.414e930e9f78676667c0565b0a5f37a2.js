3c33b2b9b5696a6f08eb5971f2582fd98835e194
Fasasi Toheeb <tohebayomide11@gmail.com>
2021-05-18 18:29:07 +0100

    Updates

---
+++ precache-manifest.414e930e9f78676667c0565b0a5f37a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a25e86e6e9c4ded942738e32052269",
+    "url": "/evolv-challenge/index.html"
+  },
+  {
+    "revision": "d00fde142f45b5c15306",
+    "url": "/evolv-challenge/static/css/main.934a7de5.chunk.css"
+  },
+  {
+    "revision": "4f6f9441dda4ab81b5e8",
+    "url": "/evolv-challenge/static/js/2.2e9339f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/evolv-challenge/static/js/2.2e9339f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00fde142f45b5c15306",
+    "url": "/evolv-challenge/static/js/main.358e8d3d.chunk.js"
+  },
+  {
+    "revision": "2525de73a29f134f155e",
+    "url": "/evolv-challenge/static/js/runtime-main.8ba313c0.js"
+  }
+]);
